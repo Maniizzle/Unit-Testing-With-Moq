875392799d99eb7f4d2ebc540cd0999a19d0eded
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2021-05-04 21:49:38 +0100

    inital moq test

---
+++ CreditCardApplication.Test/CreditCardAllicationEvaluatesShould.cs
@@ -0,0 +1,30 @@
+using CreditCardApplications;
+using System;
+using Xunit;
+
+namespace CreditCardApplications.Test
+{
+    public class CreditCardAllicationEvaluatesShould
+    {
+        [Fact]
+        public void AcceptHighIncomeApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication { GrossAnnualIncome = 100_000 };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication { Age = 19 };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+    }
+}
---
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,40 @@
+﻿namespace CreditCardApplications
+{
+    public class CreditCardApplicationEvaluator
+    {
+        public CreditCardApplicationEvaluator(IFrequentFlyerNumberValidator validator)
+        {
+            this.validator = validator;
+        }
+
+        private const int AutoReferralMaxAge = 20;
+        private const int HighIncomeThreshold = 100_000;
+        private const int LowIncomeThreshold = 20_000;
+        private readonly IFrequentFlyerNumberValidator validator;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= HighIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+
+            var isValidFreuentFlyMember = validator.IsValid(application.FrequentFlyerNumber);
+            if (!isValidFreuentFlyMember)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+            if (application.Age <= AutoReferralMaxAge)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+
+            if (application.GrossAnnualIncome < LowIncomeThreshold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+
+            return CreditCardApplicationDecision.ReferredToHuman;
+        }       
+    }
+}
