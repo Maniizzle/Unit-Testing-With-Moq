b97b64fe475c1b0687ebc98359c82bec2acab71c
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2021-05-05 23:56:45 +0100

    using moq argument matching

--- CreditCardApplication.Test/CreditCardAllicationEvaluatesShould.cs
+++ CreditCardApplication.Test/CreditCardAllicationEvaluatesShould.cs
@@ -1,6 +1,7 @@
 using CreditCardApplications;
 using System;
 using Xunit;
+using Moq;
 
 namespace CreditCardApplications.Test
 {
@@ -9,7 +10,8 @@ namespace CreditCardApplications.Test
         [Fact]
         public void AcceptHighIncomeApplications()
         {
-            var sut = new CreditCardApplicationEvaluator();
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>(); 
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication { GrossAnnualIncome = 100_000 };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
@@ -20,11 +22,40 @@ namespace CreditCardApplications.Test
         [Fact]
         public void ReferYoungApplications()
         {
-            var sut = new CreditCardApplicationEvaluator();
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication { Age = 19 };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            
+            //setting the isvalid method
+           // mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+          
+            //Argument Matching
+            //return true when any string is passed to the method
+            //mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
+            
+            //return true to a particular scenario
+           // mockValidator.Setup(x => x.IsValid(It.Is<string>(numb=>numb.StartsWith("y")))).Returns(true);
+          
+            //return true if it is in a range
+            mockValidator.Setup(x => x.IsValid(It.IsInRange<string>("a","z",Moq.Range.Inclusive))).Returns(true);
+          
+            
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+             var application = new CreditCardApplication { GrossAnnualIncome = 19_999,
+            Age=42,FrequentFlyerNumber="x"};
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
         }
     }
 }
---
+++ CreditCardApplications/testtt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditCardApplications
+{
+     struct AnimationStates
+    {
+        public static Dictionary<string, string[]> items = new Dictionary<string, string[]>
+        {
+            {"Idle_unarmed",new string[]{ "Knife_equip", "Pistol_equip", "Rifle_equip", "Idle_unarmed" }  }
+        };
+
+
+
+        public static string[] Idle_unarmed = { "Knife_equip", "Pistol_equip", "Rifle_equip", "Idle_unarmed" };
+
+
+        public static string[] Pistol_equip = { "Pistol_idle" };
+
+        public static string[] Pistol_fire = { "Pistol_idle" };
+        public static string[] Pistol_idle = { "Pistol_fire", "Pistol_reload", "Pistol_unequip", "Pistol_idle" };
+        public static string[] Pistol_reload = { "Pistol_idle" };
+        public static string[] Pistol_unequip = { "Idle_unarmed" };
+
+        public static string[] Rifle_equip = { "Rifle_idle" };
+        public static string[] Rifle_fire = { "Rifle_idle" };
+        public static string[] Rifle_idle = new string[4] { "Rifle_fire", "Rifle_reload", "Rifle_unequip", "Rifle_idle" };
+        public static string[] Rifle_reload = { "Rifle_idle" };
+        public static string[] Rifle_unequip = { "Idle_unarmed" };
+
+        public static string[] Knife_equip = { "Knife_idle" };
+        public static string[] Knife_fire = { "Knife_idle" };
+        public static string[] Knife_idle = { "Knife_fire", "Knife_unequip", "Knife_idle" };
+        public static string[] Knife_unequip = { "Knife_unarmed" };
+        public static bool operator ==(AnimationStates anim1, AnimationStates anim2)
+        {
+            return anim1.Equals(anim2);
+        }
+
+        public static bool operator !=(AnimationStates anim1, AnimationStates anim2)
+        {
+            return !anim1.Equals(anim2);
+        }
+
+        public string[] this[string state] => FindStateIndex(state);
+
+        private string[] FindStateIndex(string state)
+        {
+
+            foreach (var item in items)
+            {
+                if (item.Key == state)
+                {
+                    return item.Value;
+                }
+
+            }
+            throw new ArgumentOutOfRangeException(
+             nameof(state),
+             $"State {state} is not supported.");
+
+        }
+
+
+
+        public string animationStates { get; set; }
+    };
+}
