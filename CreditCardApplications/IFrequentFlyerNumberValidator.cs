18cae84a59c91ff696dfba0aeaf0c4d8cd7d1824
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2021-05-13 23:40:57 +0100

    update test

--- CreditCardApplications/IFrequentFlyerNumberValidator.cs
+++ CreditCardApplications/IFrequentFlyerNumberValidator.cs
@@ -7,4 +7,9 @@ namespace CreditCardApplications
         bool IsValid(string frequentFlyerNumber);
         void IsValid(string frequentFlyerNumber, out bool isValid);
     }
+
+    public interface IGateway
+    {
+        int Execute(ref Person person)
+    }
 }
